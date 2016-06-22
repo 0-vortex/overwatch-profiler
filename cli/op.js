e50339dbba259c3a76f913deca6e1bc2c3bf201a
TED Vortex <ted.vortex@gmail.com>
2016-06-22 05:10:04 +0100

    Modularised for local storage

--- cli/op.js
+++ cli/op.js
@@ -7,7 +7,7 @@
 var program = require('commander');
 
 program
-    .version('0.0.1')
+    .version('0.2.3')
     .command('search [tag]', 'search for a battle.net tag and get available platforms')
     .command('parse [tag] [region] [platform]', 'get profile data for a battle.net tag, region and platform');
 
